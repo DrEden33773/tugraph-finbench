68aee5e23761ebdcde42a5bd77af046b23d4b4d6
Eden Wang <edwardwang33773@gmail.com>
2023-11-23 14:16:56 +0800

    try to deal with `Case2`

---
+++ src/main/java/org/ember/TuGraphFinbench/Exec/Case2.java
@@ -0,0 +1,91 @@
+package org.ember.TuGraphFinbench.Exec;
+
+import com.antgroup.geaflow.api.function.io.SinkFunction;
+import com.antgroup.geaflow.api.graph.PGraphWindow;
+import com.antgroup.geaflow.api.pdata.stream.window.PWindowSource;
+import com.antgroup.geaflow.api.window.impl.AllWindow;
+import com.antgroup.geaflow.common.config.Configuration;
+import com.antgroup.geaflow.env.Environment;
+import com.antgroup.geaflow.example.function.FileSink;
+import com.antgroup.geaflow.example.util.EnvironmentUtil;
+import com.antgroup.geaflow.example.util.PipelineResultCollect;
+import com.antgroup.geaflow.example.util.ResultValidator;
+import com.antgroup.geaflow.model.common.Null;
+import com.antgroup.geaflow.model.graph.edge.IEdge;
+import com.antgroup.geaflow.model.graph.edge.impl.ValueEdge;
+import com.antgroup.geaflow.model.graph.vertex.IVertex;
+import com.antgroup.geaflow.model.graph.vertex.impl.ValueVertex;
+import com.antgroup.geaflow.pipeline.IPipelineResult;
+import com.antgroup.geaflow.pipeline.Pipeline;
+import com.antgroup.geaflow.pipeline.PipelineFactory;
+import com.antgroup.geaflow.pipeline.task.PipelineTask;
+import com.antgroup.geaflow.view.GraphViewBuilder;
+import com.antgroup.geaflow.view.IViewDesc;
+import com.antgroup.geaflow.view.graph.GraphViewDesc;
+import org.ember.TuGraphFinbench.Algorithms.Case2Algorithm;
+import org.ember.TuGraphFinbench.DataLoader;
+import org.ember.TuGraphFinbench.Env.Env;
+import org.ember.TuGraphFinbench.Record.Case2Vertex;
+import org.ember.TuGraphFinbench.Record.RawEdge;
+import org.ember.TuGraphFinbench.Record.RawVertex;
+import org.ember.TuGraphFinbench.Source.DataSource;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Case2 {
+    public static final String RESULT_FILE_PATH = "./target/tmp/data/result/finbench/case2";
+
+    static List<IVertex<Long, Case2Vertex>> verticesRecord = DataLoader.rawVertices.stream()
+            .map(RawVertex::toCase2Vertex)
+            .filter(Objects::nonNull)
+            .map(vertex -> new ValueVertex<>(vertex.getID(), vertex)).collect(Collectors.toList());
+
+    static List<IEdge<Long, Null>> edgesRecord = DataLoader.rawEdges.stream()
+            .map(RawEdge::toCase2Edge)
+            .filter(Objects::nonNull)
+            .map(edge -> new ValueEdge<>(edge.getSrcID(), edge.getDstID(), new Null())).collect(Collectors.toList());
+
+    static IPipelineResult<?> submit(Environment environment) {
+        Pipeline pipeline = PipelineFactory.buildPipeline(environment);
+        Configuration envConfig = environment.getEnvironmentContext().getConfig();
+        envConfig.put(FileSink.OUTPUT_DIR, RESULT_FILE_PATH);
+        ResultValidator.cleanResult(RESULT_FILE_PATH);
+
+        pipeline.submit((PipelineTask) pipelineTaskCtx -> {
+            PWindowSource<IVertex<Long, Case2Vertex>> vertices = pipelineTaskCtx
+                    .buildSource(new DataSource<>(verticesRecord), AllWindow.getInstance())
+                    .withParallelism(Env.PARALLELISM_MAX);
+
+            PWindowSource<IEdge<Long, Null>> edges = pipelineTaskCtx
+                    .buildSource(new DataSource<>(edgesRecord), AllWindow.getInstance())
+                    .withParallelism(Env.PARALLELISM_MAX);
+
+            GraphViewDesc graphViewDesc = GraphViewBuilder
+                    .createGraphView("Case2Graph")
+                    .withShardNum(Env.PARALLELISM_MAX)
+                    .withBackend(IViewDesc.BackendType.Memory)
+                    .build();
+
+            PGraphWindow<Long, Case2Vertex, Null> graphWindow = pipelineTaskCtx.buildWindowStreamGraph(vertices,
+                    edges, graphViewDesc);
+
+            SinkFunction<IVertex<Long, Case2Vertex>> sink = new FileSink<>();
+
+            graphWindow.compute(new Case2Algorithm(4))
+                    .compute(Env.PARALLELISM_MAX)
+                    .getVertices()
+                    .sink(sink);
+        });
+
+        return pipeline.execute();
+    }
+
+    public static void main(String[] args) {
+        Environment environment = EnvironmentUtil.loadEnvironment(args);
+        IPipelineResult<?> result = submit(environment);
+        PipelineResultCollect.get(result);
+        environment.shutdown();
+    }
+}
